db9f80701c9756abbcd13887a3a2266410f64119
Linet Oliech <linetaoliech@gmail.com>
2023-01-25 23:28:10 +0300

    passed Home component to main app, imported routes and passed it as a route

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../components/App.css'
 import Navbar from './Navbar';
-
+import Home from './Home';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+        </Routes>
       </header>
     </div>
   );
