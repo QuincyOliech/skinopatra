b6b545bc7218369497596ccc4e23bee489bf5eca
Linet Oliech <linetaoliech@gmail.com>
2023-01-26 08:50:24 +0300

    updated form input functionalty

--- src/pages/CreateProductForm.js
+++ src/pages/CreateProductForm.js
@@ -1,15 +1,43 @@
 import React from "react";
 
 function CreateProductForm(){
+    function handleSubmit(e){
+        // prevent reloading 
+        e.preventDefault();
+    
+        const formData={
+            title:e.target.title.value,
+            category:e.target.category.value,
+            description:e.target.description.value,
+            price:e.target.price.value,
+            image:e.target.image.value
+        };
+        fetch("http://localhost:8001/products",{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        }
+        )
+        .then(response=>response.json())
+        .then(data=>data)
+        // reset input information
+        e.target.reset()
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input name="title" type="text" placeholder="Title" required />
                 <input name="category" type="text" placeholder="Category" required/>
                 <input name="description" type="text" placeholder="Description" required/>
-                <input name="price" type="text" required  placeholder="Price"/>
-                <input name="image" type="url" placeholder="Image url"/>
+                <input name="price" type="text"  placeholder="Price" required />
+                <input name="image" type="url" placeholder="Image url" required/>
+                <button>Add Product</button>
             </form>
+            
         </div>
     )
 }
